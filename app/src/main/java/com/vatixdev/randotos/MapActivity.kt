a824b22fed92f159bf5d172f882f70efcb3f3fb5
Nini1The1best <148354724+Nini1The1best@users.noreply.github.com>
2025-09-26 17:31:41 +0200

    Update MapActivity.kt (last one was an error)

--- app/src/main/java/com/vatixdev/randotos/MapActivity.kt
+++ app/src/main/java/com/vatixdev/randotos/MapActivity.kt
@@ -1,12 +1,41 @@
-<?xml version="1.0" encoding="utf-8"?>
-<FrameLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent">
+package com.vatixdev.randotos
 
-    <fragment
-        android:id="@+id/map"
-        android:name="com.google.android.gms.maps.SupportMapFragment"
-        android:layout_width="match_parent"
-        android:layout_height="match_parent" />
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-</FrameLayout>
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private var steps: Int = 6
+    private var amplitude: Double = 0.001
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        // Récupération des extras
+        steps = intent.getIntExtra("steps", 6)
+        amplitude = intent.getDoubleExtra("amplitude", 0.001)
+
+        // Initialiser la carte
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val start = LatLng(48.8566, 2.3522) // Paris par défaut
+        mMap.addMarker(MarkerOptions().position(start).title("Départ"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(start, 14f))
+
+        // Ici tu pourras générer ton chemin aléatoire avec steps + amplitude
+    }
+}
